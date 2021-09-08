e1c712bf7cd5dd9a7ad4dee32b69ac6ac63abe8f
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-09-08 17:03:21 +0200

    programma

--- esercizio 5/Js/script.js
+++ esercizio 5/Js/script.js
@@ -5,3 +5,34 @@ quanto il numero di elementi dell’array).
 La funzione ritornerà un nuovo array con i valori che hanno la posizione
 compresa tra “a” e “b”
 */
+
+
+/*
+Function
+*/
+
+function myFunction(arr, num1, num2) {
+
+    var newArr = [];
+
+    for ( i=0; i<arr.length; i++ ) {
+
+        if ( i > num1 && i < num2 ) {
+            newArr.push(arr[i]);
+        }
+
+    }
+
+    return newArr
+}
+
+// Input
+var array = ['a', 'b', 'c', 'd', 'e', 'f'];
+var NumA = 2;
+var NumB = 5;
+
+// Programma - richiamo la funzione
+var NewArray = (myFunction(array, NumA, NumB));
+
+// Output
+console.log(NewArray);
