85460e6ae75f3b37de88a45cfe8faaf337640c95
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-09-08 12:27:52 +0200

    parola al contrario

---
+++ esercizio 3/Js/script.js
@@ -0,0 +1,21 @@
+/*
+Scrivi una funzione che accetti una stringa come argomento e la ritorni
+girata (es. Ciao -> oaiC)
+*/
+
+// function
+function reverseString(string) {
+    var supporto = '';
+    for (var i = string.length - 1; i >= 0; i--) { // var i =6-1; i
+        supporto += string[i];  // "" = "" + ultima lettera di string
+    }
+    return supporto
+}
+
+// parola utente
+var parola = prompt('Inserisci una parola');
+console.log(parola);
+
+// richiamo la funzione
+var parolaAlContrario = reverseString(parola);
+console.log(parolaAlContrario);
